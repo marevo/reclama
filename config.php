010d3aac39cdb6c9e81b8e3f0ddf36b0f67c0959
Михаил Рево <marevo1972@gmail.com>
2017-08-25 12:21:22 +0300

    add config.php file to projectVersion

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marevo
+ * Date: 09.06.2017
+ * Time: 16:27
+ */
+return[
+    'host'=>'127.0.0.1',
+    'dbname'=> 'reclama',
+    'user'=> 'root',
+    'password'=> ''
+];
